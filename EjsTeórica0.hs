d4d065407525136d2d8c58696403dd4b7e686def
Mirunr <miriam.enr0@gmail.com>
2026-08-18 18:32:35 -0300

    Update EjsTeórica0.hs
    empecé a resolver los ejemplos de la teórica 0

--- EjsTeórica0.hs
+++ EjsTeórica0.hs
@@ -1,1 +1,16 @@
+data Dirección = Norte | Este | Sur | Oeste
 
+opuesta :: Dirección -> Dirección 
+opuesta Norte = Sur 
+opuesta Sur = Norte 
+opuesta Este = Oeste 
+opuesta Oeste = Este
+
+{--el´Ultimo´IndiceDe :: Eq a => a -> [a] -> Int
+que dado un elemento x y una lista de elementos xs, devuelve el
+´ındice de la ´ultima ocurrencia de x en xs.
+Hacerla total usando el tipo Maybe --}
+
+elUltimoIndiceDe :: Eq a => a -> [a] -> Maybe Int 
+elUltimoIndiceDe _ [] = Nothing 
+elUltimoIndiceDe n (x:xs) = 

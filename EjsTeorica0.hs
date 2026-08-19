339e8ad2fa6d41ecbace5c404b3fbf4aa9d5e7dd
Mirunr <miriam.enr0@gmail.com>
2026-08-19 16:47:23 -0300

    Casi termine ejemplos de la teórica 0, solo me faltan dos funciones difíciles sobre listas

--- EjsTeorica0.hs
+++ EjsTeorica0.hs
@@ -6,15 +6,53 @@ opuesta Sur = Norte
 opuesta Este = Oeste 
 opuesta Oeste = Este
 
-{--elUltimoIndiceDe :: Eq a => a -> [a] -> Int
+{-- elUltimoIndiceDe :: Eq a => a -> [a] -> Int
 que dado un elemento x y una lista de elementos xs, devuelve el
 índice de la última ocurrencia de x en xs.
 Hacerla total usando el tipo Maybe --}
 
-elUltimoIndiceDe :: Eq a => a -> [a] -> Maybe Int 
-elUltimoIndiceDe _ [] = Nothing 
-elUltimoIndiceDe n (x:xs) = 
+elUltimoIndiceDe :: Int -> [Int] -> Maybe Int 
+elUltimoIndiceDe n lista = if null (listaDeApConIndice n 0 lista) then Nothing else Just (maximum (listaDeApConIndice n 0 lista))
 
-indiceSiguiente :: Eq a, Num b => a -> b -> [a] -> b
-indiceSiguiente x iact [] = iact
-indiceSiguiente x iact (e:es) = if x == e then iact else indiceSiguiente x iact+1 es 
+listaDeApConIndice :: Int -> Int -> [Int] -> [Int]
+listaDeApConIndice _ _ [] = []
+listaDeApConIndice elem iAct (x:xs) = if elem == x then iAct : listaDeApConIndice elem (iAct+1) xs else listaDeApConIndice elem (iAct+1) xs
+
+data AB a = Nil | Bin (AB a) a (AB a)
+
+preorder :: AB a -> [a]
+preorder Nil = []
+preorder (Bin i r d) =  [r] ++ preorder i ++ preorder d 
+
+inorder :: AB a -> [a] 
+inorder Nil = []
+inorder (Bin i r d) = inorder i ++ [r] ++ inorder d 
+
+postorder :: AB a -> [a]
+postorder Nil = []
+postorder (Bin i r d) = postorder d ++ postorder i ++ [r]
+
+data Conj a = CConj [a] deriving Show
+
+vacio :: Conj a
+vacio = CConj []
+
+insertar :: Eq a => a-> Conj a-> Conj a
+insertar e (CConj c) = if elem e c then CConj c else CConj (e:c)
+
+pertenece :: Eq a => a -> Conj a -> Bool
+pertenece e (CConj c) = elem e c 
+
+eliminar :: Eq a => a -> Conj a -> Conj a
+eliminar e (CConj c) = CConj (quitar e c)
+
+quitar :: Eq a => a -> [a] -> [a]
+quitar e [] = []
+quitar e (x:xs) = if e == x then xs else e : quitar e xs
+
+{--data Dict k v = CDict (AB (k, v)) deriving Show 
+
+vacio1 :: Dict k v
+vacio1 = CDict Nil--}
+
+subsecuencias
